18099c3c7f59e5970952f18d87f59c5579a88324
randysd <rdykstra1@yahoo.com>
2026-08-20 01:24:15 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wl-frontier-director-v61';
+const CACHE_NAME = 'wl-frontier-director-v62';
 const APP_SHELL = [
   './', './index.html', './manifest.webmanifest', './css/styles.css', './js/app.js', './assets/icons/icon.svg',
   './assets/images/triggers/bandit.svg', './assets/images/triggers/prospect.svg', './assets/images/triggers/item.svg',
